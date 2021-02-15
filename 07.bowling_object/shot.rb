8b154c55d9f9361df47ad298a3ff424579d32e89
yana-gi <yanagisawa.hika@gmail.com>
2021-02-15 15:18:45 +0900

    shotクラス修正
    attr_accessorをattr_readerへ変更
    scoreメソッドをcalculate_scoreへ名称変更
    score変数を追加

--- 07.bowling_object/shot.rb
+++ 07.bowling_object/shot.rb
@@ -1,15 +1,14 @@
+# frozen_string_literal: true
+
 class Shot
-  attr_accessor :mark
+  attr_reader :mark, :score
 
   def initialize(mark)
     @mark = mark
+    @score = calculate_score
   end
 
-  def score
-    if @mark == 'X'
-      10
-    else
-      @mark.to_i
-    end
+  def calculate_score
+    @mark == 'X' ? 10 : @mark.to_i
   end
 end
