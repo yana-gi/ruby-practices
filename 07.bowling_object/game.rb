f61f95437f4b2d0ed71ae3a4acce3f576b919500
yana-gi <yanagisawa.hika@gmail.com>
2021-02-15 15:21:16 +0900

    gameクラス修正
    attr_accessorをattr_readerへ変更
    scoreメソッドをcalculate_scoreへ名称変更
    score変数を追加

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -1,50 +1,54 @@
+# frozen_string_literal: true
+
 class Game
-  GAME_FLAME_NUMBER = 10
-  attr_accessor :result, :frames
+  NUM_FLAMES = 10
+  attr_reader :frames, :score
 
   def initialize(result)
-    @result = result
-    @frames = result_to_frames
+    @frames = create_frames(result.chars)
+    @score = calculate_score
   end
 
-  def result_to_frames
-    results = @result.chars
+  def create_frames(results)
     count = 0
     frames = []
 
-    GAME_FLAME_NUMBER.times do |i|
+    NUM_FLAMES.times do |i|
       frame = []
       frame << results[count]
 
-      if results[count] == 'X' && i != (GAME_FLAME_NUMBER - 1)
+      if results[count] == 'X' && i != (NUM_FLAMES - 1)
         count += 1
       else
         count += 1
         frame << results[count]
         count += 1
-        frame << results[count] if i == (GAME_FLAME_NUMBER - 1)
+        frame << results[count] if i == (NUM_FLAMES - 1)
       end
       frames << frame
     end
-    frames.map do |i|
-      Flame.new(i[0], i[1], i[2])
+
+    frames.map do |frame|
+      Flame.new(frame[0], frame[1], frame[2])
     end
   end
 
-  def score
+  def calculate_score
     point = 0
-    @frames.each_with_index do |frame, i|
-      point += frame.score
-      break if i == (GAME_FLAME_NUMBER - 1)
+    NUM_FLAMES.times do |i|
+      point += @frames[i].score
+      # 10フレーム目はストライク・スペアの計算をしない
+      break if i == (NUM_FLAMES - 1)
 
-      if frame.strike?
+      if @frames[i].strike?
         point += @frames[i + 1].first_shot.score
-        point += if @frames[i + 1].strike? && i != (GAME_FLAME_NUMBER - 2)
+        # 次のフレームがストライクの場合はその次のフレームを加算（9フレーム目の場合を除く）
+        point += if @frames[i + 1].strike? && i != (NUM_FLAMES - 2)
                    @frames[i + 2].first_shot.score
                  else
                    @frames[i + 1].second_shot.score
                  end
-      elsif frame.spare?
+      elsif @frames[i].spare?
         point += @frames[i + 1].first_shot.score
       end
     end
